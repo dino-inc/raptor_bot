6a59f37f5e0af8208950113283820818596e389b
dino-inc <ethan8960@gmail.com>
2020-10-16 00:21:25 -0700

    Added sounds folder

--- cogs/soundboard.py
+++ cogs/soundboard.py
@@ -3,10 +3,17 @@ from discord.ext import commands
 class Soundboard(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.voiceclient = None
 
     @commands.command()
-    async def beep(self, ctx):
-        await ctx.send("Beep.")
+    async def connect(self, ctx, *, channel : discord.VoiceChannel):
+        self.voiceclient = await channel.connect()
+
+    @commands.command()
+    async def disconnect(self, ctx):
+        await self.voiceclient.disconnect()
+
+    
 
 def setup(bot):
     bot.add_cog(Soundboard(bot))
